fa46cc354afa71aac591951f32ea8a0dff946687
Jackson Moraes <jacksontadeu@hotmail.com>
2025-07-04 19:08:01 -0300

    :sparkles: feat: Criacao do projeto Spring

---
+++ src/test/java/com/jtmjinfo/livrariaapi/LivrariaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtmjinfo.livrariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
